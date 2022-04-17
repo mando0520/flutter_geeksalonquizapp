c96fd26341d02d0b2cfd9777855f3b1ad98c79d9
mando0520 <y2pgftpe@gmail.com>
2022-04-17 22:22:15 +0900

    サンプル画面初期

---
+++ lib/start_page.dart
@@ -0,0 +1,60 @@
+// ignore_for_file: prefer_const_constructors, deprecated_member_use, use_key_in_widget_constructors
+
+import "package:flutter/material.dart";
+import "package:geeksalonquizapp/question_page.dart";
+
+class Start extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.orange,
+          ),
+          // backgroundColor: Colors.white,
+          body: SafeArea(
+            child: StartPage(),
+          )
+      )
+    );
+  }
+}
+
+class StartPage extends StatefulWidget {
+  @override
+  _StartPageState createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        onPressed: () {
+          Navigator.push(
+            this.context,
+            MaterialPageRoute(
+              builder: (context) => Question()
+            ),
+          );
+        },
+        child: Text(
+          "はじめる", 
+          style: TextStyle(
+            fontSize: 20.0)
+        ),
+        color: Colors.green,
+        elevation: 15.0,
+        padding: EdgeInsets.symmetric(
+          vertical: 15.0, 
+          horizontal: 100.0
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            15.0
+          )
+        )
+      )
+    );
+  }
+}

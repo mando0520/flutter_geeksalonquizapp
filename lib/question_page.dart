5327accda9671af3f01e9ad1cda6825532be2139
mando0520 <y2pgftpe@gmail.com>
2022-05-22 01:58:55 +0900

    dialogデザイン

--- lib/question_page.dart
+++ lib/question_page.dart
@@ -72,140 +72,168 @@ class _QuestionState extends State<Question> {
   Widget build(BuildContext context) {
     print(quiz_statements);
     return quiz_statements.isEmpty
-    ? CircularProgressIndicator()
-    : MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "GeekSalonQuiz",
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-      ),
-        home: Scaffold(
-          body: Padding(
-            padding: EdgeInsets.only(top: 30, bottom: 30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/top.png',height: 200),
-                Expanded(
-                  flex: 1,
-                  child: Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Text(
-                      quiz_statements[questionNumber],
-                      style: TextStyle(
-                        fontSize: 22.0,
-                      ),
-                    )
-                  )
-                ),
+        ? CircularProgressIndicator()
+        : MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: "GeekSalonQuiz",
+            theme: ThemeData(
+              scaffoldBackgroundColor: Colors.white,
+            ),
+            home: Scaffold(
+              body: Padding(
+                  padding: EdgeInsets.only(top: 30, bottom: 30),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset('assets/images/top.png', height: 200),
+                        Expanded(
+                            flex: 1,
+                            child: Padding(
+                                padding: const EdgeInsets.all(15.0),
+                                child: Text(
+                                  quiz_statements[questionNumber],
+                                  style: TextStyle(
+                                    fontSize: 22.0,
+                                  ),
+                                ))),
+                        Expanded(
+                            child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                              ButtonTheme(
+                                minWidth: 120.0,
+                                height: 120.0,
+                                child: RaisedButton(
+                                    onPressed: () {
+                                      bool correctAnswer =
+                                          answers[questionNumber];
 
-                Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ButtonTheme(
-                          minWidth: 120.0,
-                          height: 120.0,
-                          child:  RaisedButton(
-                            onPressed: () {
-                              bool correctAnswer = answers[questionNumber];
+                                      // 正解だった場合、正解数に1を足す
+                                      // 音を鳴らす
+                                      if (correctAnswer) {
+                                        numberOfCorrectAnswers++;
+                                        _player
+                                            .play('sounds/correct_buzzer.mp3');
+                                      } else {
+                                        _player.play('sounds/wrong_buzzer.mp3');
+                                      }
 
-                              // 正解だった場合、正解数に1を足す
-                              // 音を鳴らす
-                              if (correctAnswer) {
-                                numberOfCorrectAnswers++;
-                                _player.play('sounds/correct_buzzer.mp3');
-                              } else {
-                                _player.play('sounds/wrong_buzzer.mp3');
-                              }
+                                      // ダイアログを出す
+                                      openDialog(
+                                          context,
+                                          correctAnswer,
+                                          answer_statements[questionNumber],
+                                          true);
+                                    },
+                                    child: Text("○",
+                                        style: TextStyle(
+                                          fontSize: 70.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        )),
+                                    color: Colors.red.withOpacity(0.8),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(30.0))),
+                              ),
+                              ButtonTheme(
+                                  minWidth: 120.0,
+                                  height: 120.0,
+                                  child: RaisedButton(
+                                      onPressed: () {
+                                        bool correctAnswer =
+                                            answers[questionNumber];
 
-                              // ダイアログを出す
-                              openDialog(context, correctAnswer,
-                                  answer_statements[questionNumber], true);
-                            },
-                            child: Text("○", style: TextStyle(
-                              fontSize: 70.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              )
-                            ),
-                            color: Colors.red.withOpacity(0.8),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30.0))
-                          ),
-                        ),
-                        ButtonTheme(
-                        minWidth: 120.0,
-                        height: 120.0,
-                        child: RaisedButton(
-                          onPressed: () {
-                            bool correctAnswer = answers[questionNumber];
+                                        // 不正解だった場合、正解数に1を足す
+                                        // 音を鳴らす
+                                        if (!correctAnswer) {
+                                          numberOfCorrectAnswers++;
+                                          _player.play(
+                                              'sounds/correct_buzzer.mp3');
+                                        } else {
+                                          _player
+                                              .play('sounds/wrong_buzzer.mp3');
+                                        }
 
-                            // 不正解だった場合、正解数に1を足す
-                            // 音を鳴らす
-                            if (!correctAnswer) {
-                              numberOfCorrectAnswers++;
-                              _player.play('sounds/correct_buzzer.mp3');
-                            } else {
-                              _player.play('sounds/wrong_buzzer.mp3');
-                            }
-                            
-                            // ダイアログを出す
-                            openDialog(context, correctAnswer,
-                                answer_statements[questionNumber], false);
-                          },
-                          child: Text("✕", style: TextStyle(
-                            fontSize: 70.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            )
-                          ),
-                          color: Colors.blue.withOpacity(0.8),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30.0))
-                          )
-                        )
-                    ]
-                  )
-                )
-              ]      
-            )
-        ),
-      )
-    );
+                                        // ダイアログを出す
+                                        openDialog(
+                                            context,
+                                            correctAnswer,
+                                            answer_statements[questionNumber],
+                                            false);
+                                      },
+                                      child: Text("✕",
+                                          style: TextStyle(
+                                            fontSize: 70.0,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white,
+                                          )),
+                                      color: Colors.blue.withOpacity(0.8),
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(30.0))))
+                            ]))
+                      ])),
+            ));
   }
 
   void openDialog(BuildContext context, correctAnswer, answer_statement, ans) {
     var answer_text = "不正解！";
+    var ans_color = Colors.blue;
     if (correctAnswer == ans) {
       answer_text = "正解！";
+      ans_color = Colors.red;
     }
 
     showDialog(
       context: context,
       // ignore: unnecessary_new
-      builder: (BuildContext context) => new SimpleDialog(
-        title: new Text(answer_text),
-        children: <Widget>[
-          Text(answer_statement),
-          SimpleDialogOption(
+      builder: (BuildContext context) => new AlertDialog(
+        title: Text(
+          answer_text,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: ans_color,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        content: Text(answer_statement),
+        
+        actions: <Widget>[
+          ButtonTheme(
+            minWidth: 60.0,
+            height: 35.0,
+            child: Center(child: 
+            RaisedButton(
               onPressed: () {
                 Navigator.pop(context);
 
-                // 問題番号が問題数未満の場合、問題数に1を足す
-                if (questionNumber + 1 < quiz_statements.length) {
-                  setState(() {
-                    questionNumber++;
-                  });
-                } else {
-                  Navigator.push(
+                  // 問題番号が問題数未満の場合、問題数に1を足す
+                  if (questionNumber + 1 < quiz_statements.length) {
+                    setState(() {
+                      questionNumber++;
+                    });
+                  } else {
+                    Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => Result(
-                              numberOfCorrectAnswers: numberOfCorrectAnswers)));
-                }
-              },
-              child: Text("Continue"))
+                        builder: (context) => Result(
+                          numberOfCorrectAnswers: numberOfCorrectAnswers)));
+                  }
+                },
+              child: Text("Continue",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  )),
+              color: Color.fromARGB(255, 58, 255, 64),
+              shape: RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.circular(20.0))),
+            )
+          )
         ],
       ),
     );
